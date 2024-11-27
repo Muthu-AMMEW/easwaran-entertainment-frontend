8b42b040de41e2600adb087c51fe225526543f20
Muthu-AMMEW <muthu.ammew@gmail.com>
2024-11-27 22:07:52 +0530

    login Register finished

--- src/services/Api.js
+++ src/services/Api.js
@@ -4,16 +4,20 @@ import { getUserData} from './Storage';
 axios.defaults.baseURL = "https://identitytoolkit.googleapis.com/v1";
 const API_KEY = "AIzaSyBOne58CDodisdw3RnMU9W26GLnJZxPcPk";
 const REGISTER_URL = `/accounts:signUp?key=${API_KEY}`;
-const LOGIN_URL = `/accounts:signInWithPassword?key=${API_KEY}`;
+// const LOGIN_URL = `/accounts:signInWithPassword?key=${API_KEY}`;
+const login = process.env.REACT_APP_API_URL+'/login'
 const USER_DETAILS_URL = `/accounts:lookup?key=${API_KEY}`;
 
 export const RegisterApi = (inputs)=>{
     let data  = {displayName:inputs.name,email:inputs.email,password:inputs.pwd }
     return axios.post(REGISTER_URL,data)
-}
+// export const LoginApi = (inputs)=>{
+//     let data  = {email:inputs.email,password:inputs.pwd }
+//     return axios.post(LOGIN_URL,data)
+// }
+
 export const LoginApi = (inputs)=>{
-    let data  = {email:inputs.email,password:inputs.pwd }
-    return axios.post(LOGIN_URL,data)
+    return axios.post(login,inputs)
 }
 export const UserDetailsApi = ()=>{
     let data = {idToken:getUserData()}
