ccffd5ee67149d1a47a1236e1608ced8408d8f2d
Muthu-AMMEW <muthu.ammew@gmail.com>
2024-12-22 11:46:23 +0530

    updates

--- src/pages/admin/Dashboard.jsx
+++ src/pages/admin/Dashboard.jsx
@@ -1,13 +1,19 @@
+import { Navigate } from 'react-router-dom'
+import { isAuthenticated, isAdmin } from '../../services/Auth'
 import AdminNavBar from './AdminNavBar'
 import AllVideoDetails from './AllVideoDetails'
 
 export default function Dashboard() {
+  if (!isAuthenticated() || !isAdmin()) {
+    return <Navigate to="/login" />
+  }
+
   return (
     <>
-    <div>
+      <div>
         <AdminNavBar />
         <AllVideoDetails />
-    </div>
+      </div>
     </>
   )
 }
